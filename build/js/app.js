66997f0a4a57e1435edea519326e66e13d459933
unscript <alexkh34@gmail.com>
2017-02-05 15:10:13 +0300

    add data attr

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,30 @@
 console.log("Z DATE CALC V0.0.1");
 
 window.onload = function () {
-    $(".input-block__control").on("click", function () {
-        console.log("CLICK");
-    })
+    var inputBlock = $("#input-block");
+
+    inputBlock.on("mousedown", function (e) {
+        console.log("click");
+        e.preventDefault();
+    });
+
+    inputBlock.on("dblclick", function (e) {
+        console.log("dblclick");
+        e.preventDefault();
+    });
+
+    inputBlock.on("selectstart", function (e) {
+        console.log("selectstart");
+        e.preventDefault();
+    });
+
+    window.onclick = function (e) {
+        e.preventDefault();
+    }
+
+    window.ondblclick = function (e) {
+        e.preventDefault();
+    }
+
+
 }
